f9941a83b0b750420302a85e137026af871c97c5
MaryCurious <103515117+MaryCurious@users.noreply.github.com>
2022-04-21 19:19:40 +0300

    Update all_hw.js

--- all_hw.js
+++ all_hw.js
@@ -24,7 +24,7 @@ let age_2 = 18
 let age_3 = 60
 
 if (age_1 < age_2){
-    console.log("You don’t have access cause your age is" + age_2 + "It’s less then")
+    console.log("You don’t have access cause your age is" + age_1 + "It’s less then")
 }
 else if (age_1 >= age_2 && age_1 <  age_3){
     console.log("Welcome!")
